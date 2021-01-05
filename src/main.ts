f5efd72e6cb18b75dcc22b97b0a888e9d3dddee3
unknown <alihussaindhuniya0307@gmail.com>
2021-01-05 18:40:48 +0545

    lesson11-official docs

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
-// ts config
+//From official docs
 
-//run tsc --init or npx tsc --init
-
-// basic setup
-//include below in tsconfig.json file
-
-{
-  "compilerOptions" : {
-      "target": "ES3",
-      "module": "ES2015",
-      "declaration": false,
-      "sourceMap": true,
-      "outDir": "./dist",
-      "rootDir": "./src",
-      "watch": true
-  },
-  
- 
-  "include": [
-      "src/**/*"
-  ]
-}
+/*
+Get Started, Handbook and Handbook ref are the main topics
+ANd other section of docs are useful when it comes to specific things
+like for buiding packages in ts we need .dts file and declaration files section
+for tsconfig file we need project configuration section
+what's new section for new release
+tutorials section is for specific language like c#,asp.net and so on
+stick with basics and do read others code to find a way of writing better ts code. 
+ */

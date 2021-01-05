621055d176ed1071396f96f4d8c4c973ac88c39f
unknown <alihussaindhuniya0307@gmail.com>
2021-01-05 14:59:14 +0545

    more basic types added

--- src/main.ts
+++ src/main.ts
@@ -24,4 +24,23 @@ We may need to describe the type of variables that we do not know when we are wr
  // void
  // undefined and null
  // never
- 
+ /*
+ Type assertions
+Sometimes you’ll end up in a situation where you’ll know more about a value than TypeScript does. Usually, this will happen when you know the type of some entity could be more specific than its current type.
+
+Type assertions are a way to tell the compiler “trust me, I know what I’m doing.” A type assertion is like a type cast in other languages, but it performs no special checking or restructuring of data. It has no runtime impact and is used purely by the compiler. TypeScript assumes that you, the programmer, have performed any special checks that you need.
+
+Type assertions have two forms.
+
+One is the as-syntax:
+
+let someValue: unknown = "this is a string";
+
+let strLength: number = (someValue as string).length;Try
+The other version is the “angle-bracket” syntax:
+
+let someValue: unknown = "this is a string";
+
+let strLength: number = (<string>someValue).length;Try
+The two samples are equivalent. Using one over the other is mostly a choice of preference; however, when using TypeScript with JSX, only as-style assertions are allowed.
+  */

d023a9103639ca053ae5e00692bf4a76086a8f45
unknown <alihussaindhuniya0307@gmail.com>
2021-01-07 12:22:50 +0545

    lesson-14 what is jsdocs? way to check types and documenting js code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
-// generics
-function identity<T>(arg: T): T {
-    return arg;
-  }
+// before ts
+/*
+we use jsdocs tool before we have ts for documenting our js code
 
-  let output = identity<string>("myString");
-  console.log(output);
-  //or
-  console.log(identity(444)); /* or */ console.log(identity<number>(555));
+JSDoc is a markup language used to annotate JavaScript source code files. Using comments containing JSDoc, programmers can add documentation describing the application programming interface of the code they're creating
+
+*/
+
+/*
+sources:
+links given
+https://www.youtube.com/watch?v=Nqv6UkTROak
+https://www.youtube.com/watch?v=YK-GurROGIg
+https://jsdoc.app/
+https://devhints.io/jsdoc
+*/
+
+/*
+js docs is used for documenting code and can generate docs page in html file
+js docs is used for to check types in js code like typescript
+can be used in js,react,node,express and so on
+*/
